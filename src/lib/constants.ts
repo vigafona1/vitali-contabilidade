3e441313b2d777a81e20f4b6e2d6c0a9a83ab02a
Biel-lopes <bielopes.fsilva@gmail.com>
2026-01-29 08:40:58 -0300

    update number

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 // Constantes de contato e redes sociais
-export const WHATSAPP_NUMBER = "5561995450608";
+export const WHATSAPP_NUMBER = "5561991352509";
 export const WHATSAPP_LINK = `https://wa.me/${WHATSAPP_NUMBER}?text=Olá!%20Vim%20pelo%20site%20e%20gostaria%20de%20saber%20mais%20sobre%20os%20serviços%20da%20Vitali.`;
 
 export const INSTAGRAM_LINK = "https://www.instagram.com/vitali.contabilidade";
